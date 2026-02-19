c1dd6ce738c11978e61fc7ff843c2a71fce774d4
Zelelo622 <egorcom27@gmail.com>
2026-02-19 15:09:25 +0300

    fix: mobile verst community list

--- src/features/communities/components/CommunityList.tsx
+++ src/features/communities/components/CommunityList.tsx
@@ -25,7 +25,7 @@ export const CommunityList = ({
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
       {communities.map((community) => (
         <CommunityCard
           key={community.id}
